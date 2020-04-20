cdf57635d34108ee8cb8b8fe7fb717289d6aa6b9
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-04-20 15:48:01 +0300

    add google geocoding api

---
+++ backend/util/localtion.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+async function getCoordsForAddress(address) {
+	const response = await axios.get(
+		`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+			address
+		)}&key=${process.env.GOOGLE_PLACES_API_KEY}`
+	);
+
+	const { data } = response;
+
+	if (!data || data.status === 'ZERO_RESULTS') {
+		//TODO: handle error
+	}
+
+	const coordinates = data.results[0].geometry.location;
+	return coordinates;
+}
+
+module.exports = {
+	getCoordsForAddress,
+};
