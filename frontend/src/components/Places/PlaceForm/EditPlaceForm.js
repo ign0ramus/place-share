ebe8a8358a28f6bd21e226e6eaba18afebc221f8
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-04-23 17:11:53 +0300

    add edit and create place api to frontend

--- frontend/src/components/Places/PlaceForm/EditPlaceForm.js
+++ frontend/src/components/Places/PlaceForm/EditPlaceForm.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import isObject from 'lodash.isobject';
 
+import { useHttpClient } from '../../../hooks/httpHook';
+import { GET_OR_EDIT_PLACE_API } from '../../../const/api';
+import { MY_PLACES_URL } from '../../../const/urls';
+import { patchRequest } from '../../../util/fetch';
 import Card from '../../common/Card/Card';
 import { useForm } from '../../../hooks/formHook';
 import Form from '../../common/Form/Form';
 import Input from '../../common/Input/Input';
 import Button from '../../common/Button/Button';
-import classes from './EditPlaceForm.module.scss';
+import { UserContext } from '../../../context/UserContext';
+import classes from './styles.module.scss';
 
 const EditPlaceForm = (props) => {
 	const initInputs = {
@@ -18,14 +25,33 @@ const EditPlaceForm = (props) => {
 			id: 'description',
 		},
 	};
+	const { sendRequest } = useHttpClient(
+		GET_OR_EDIT_PLACE_API(props.place._id),
+		patchRequest
+	);
+	const [state, handleChange, setErrors] = useForm(initInputs, {});
+	const [requestError, setRequestError] = useState(null);
+	const history = useHistory();
+	const { user } = useContext(UserContext);
 
-	const [state, handleChange] = useForm(initInputs, {});
-
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+		const res = await sendRequest({
+			title: state.inputs.title.value,
+			description: state.inputs.description.value,
+		});
+
+		if (res.error) {
+			if (isObject(res.error)) {
+				return setErrors(res.error);
+			}
+
+			return setRequestError(res.error);
+		}
+		history.push(MY_PLACES_URL(user._id));
 	};
 
-	if (props.error) {
+	if (props.getPlaceError) {
 		return (
 			<div className={classes.container}>
 				<Card>
@@ -52,8 +78,9 @@ const EditPlaceForm = (props) => {
 				onChange={handleChange}
 				error={state.errors.description}
 			/>
+			{requestError && <span className={classes.error}>{requestError}</span>}
 			<Button type='submit' disabled={state.isSubmitDisabled}>
-				Edit place
+				{'Edit place'}
 			</Button>
 		</Form>
 	);
--- frontend/src/components/Places/PlaceForm/NewPlaceForm.js
+++ frontend/src/components/Places/PlaceForm/NewPlaceForm.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import isObject from 'lodash.isobject';
 
 import { useForm } from '../../../hooks/formHook';
 import Form from '../../common/Form/Form';
 import Input from '../../common/Input/Input';
 import Button from '../../common/Button/Button';
+import { ADD_NEW_PLACE_API } from '../../../const/api';
+import { MY_PLACES_URL } from '../../../const/urls';
+import { useHttpClient } from '../../../hooks/httpHook';
+import Spinner from '../../common/Spinner/Spinner';
+import { UserContext } from '../../../context/UserContext';
+import { postRequest } from '../../../util/fetch';
+import classes from './styles.module.scss';
 
-const initInputs = {
+const createInitInputs = () => ({
 	title: {
 		value: '',
 		id: 'title',
@@ -18,16 +27,43 @@ const initInputs = {
 		value: '',
 		id: 'address',
 	},
-};
+});
 
 const NewPlaceForm = () => {
-	const [state, handleChange] = useForm(initInputs, {});
+	const [state, handleChange, setErrors, resetState] = useForm(
+		createInitInputs(),
+		{}
+	);
+	const { isLoading, sendRequest } = useHttpClient(
+		ADD_NEW_PLACE_API,
+		postRequest
+	);
+	const [requestError, setRequestError] = useState(null);
+	const { user } = useContext(UserContext);
+	const history = useHistory();
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+		const res = await sendRequest({
+			title: state.inputs.title.value,
+			description: state.inputs.description.value,
+			address: state.inputs.address.value,
+		});
+
+		if (res.error) {
+			if (isObject(res.error)) {
+				return setErrors(res.error);
+			}
+
+			return setRequestError(res.error);
+		}
+		resetState(createInitInputs(), {});
+		history.push(MY_PLACES_URL(user._id));
 	};
 
-	return (
+	return isLoading ? (
+		<Spinner asOverlay />
+	) : (
 		<Form onSubmit={handleSubmit}>
 			<Input
 				id={state.inputs.title.id}
@@ -51,8 +87,9 @@ const NewPlaceForm = () => {
 				onChange={handleChange}
 				error={state.errors.address}
 			/>
+			{requestError && <span className={classes.error}>{requestError}</span>}
 			<Button type='submit' disabled={state.isSubmitDisabled}>
-				Add place
+				{'Add place'}
 			</Button>
 		</Form>
 	);
