957917c5a94ab2871448e0f51f054fd26cb3c148
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-04-24 16:27:36 +0300

    add api url

--- frontend/src/const/api.js
+++ frontend/src/const/api.js
@@ -1,12 +1,12 @@
 const API =
 	process.env.NODE_ENV === 'development'
 		? 'http://localhost:5000/api'
-		: 'https://sheltered-woodland-30586.herokuapp.com';
+		: 'https://place-share-mern.herokuapp.com';
 
 export const API_STATIC_URL =
 	process.env.NODE_ENV === 'development'
 		? 'http://localhost:5000'
-		: 'https://sheltered-woodland-30586.herokuapp.com';
+		: 'https://place-share-mern.herokuapp.com';
 
 export const SIGN_UP_API = `${API}/users/sign-up`;
 export const SIGN_IN_API = `${API}/users/sign-in`;
